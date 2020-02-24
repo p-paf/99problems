a00c396edd83aa74f14b9d637e0bdb16d83aee7b
Ishan Bhanuka <bhanuka.ishan@gmail.com>
2020-02-24 16:02:05 +0530

    Add instructions and references to README
    Rename files
    Add test expressions

--- twitu/Problem2.hs
+++ twitu/Problem2.hs
@@ -9,3 +9,7 @@ myButLast' [] = error "Cannot find but last of empty list"
 myButLast' (a:[]) = error "Cannot find but last of list containing one element"
 myButLast' (a:b:[]) = a
 myButLast' (x:xs) = myButLast' xs
+
+test1 = myButLast' ('a':'b':[])
+test2 = myButLast' []
+test3 = myButLast' [8, 7..(-9)]  -- https://stackoverflow.com/questions/6806455/decrementing-ranges-in-haskell
