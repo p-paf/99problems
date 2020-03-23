f01f3192915a7bbea3662ff680cf1300ef8652db
abhisharma76 <62178878+abhisharma76@users.noreply.github.com>
2020-03-23 13:15:59 +0530

    Update problem6.hs

--- problem6.hs
+++ problem6.hs
@@ -11,3 +11,7 @@ reverselistpass (x:xs) ans = reverselistpass xs ([x] ++ ans)
 palindromeornot :: Eq a => [a] -> Bool
 palindromeornot list = (list == (reverselist list ))
 
+
+test1 = palindromeornot [1,2,3,4,5,4,3,2,1]
+test2 = palindrmeornot "abhihba"
+
