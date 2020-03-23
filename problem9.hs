9db8932ff656a170f08aa8136ba36905a1241d4f
abhisharma76 <62178878+abhisharma76@users.noreply.github.com>
2020-03-23 13:17:25 +0530

    Update problem9.hs

--- problem9.hs
+++ problem9.hs
@@ -6,3 +6,7 @@ pack :: Eq a => [a] -> [[a]]
 pack [] = []
 pack (x:xs) = let (first, remaining) = span (== x) xs
              in (x:first) : pack remaining
+
+test1 = pack [1,1,1,2,2,2,3,3,3]
+test2 = pack "aaaaaabbbbbbbbbbccccddd"
+             
