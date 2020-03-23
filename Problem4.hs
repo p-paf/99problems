bffacd34503e47d2ab6d42cac6de8b29ba8a07bc
abhisharma76 <62178878+abhisharma76@users.noreply.github.com>
2020-03-23 13:02:58 +0530

    Update Problem4.hs

--- Problem4.hs
+++ Problem4.hs
@@ -9,3 +9,6 @@ lenlist list = lenlistpass list  0
 lenlistpass :: [a] -> Int -> Int
 lenlistpass [] n = n
 lenlistpass (x:xs) n = lenlistpass xs (n + 1)
+
+test1 = [1,2,3,4,5,67]
+test2 = "abhishek"
