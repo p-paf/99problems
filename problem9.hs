2da28a89551b063e17d2508bddcc571b3bf093bc
abhisharma76 <62178878+abhisharma76@users.noreply.github.com>
2020-03-23 13:21:07 +0530

    Update problem9.hs

--- problem9.hs
+++ problem9.hs
@@ -8,5 +8,5 @@ pack (x:xs) = let (first, remaining) = span (== x) xs
              in (x:first) : pack remaining
 
 test1 = pack [1,1,1,2,2,2,3,3,3]
-test2 = pack "aaaaaabbbbbbbbbbccccddd"
+test2 = pack '(aaaaaabbbbbbbbbbccccddd)'
              
