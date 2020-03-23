98285069fcf72bc47f7dd5ef34f881036b20b9ed
abhisharma76 <62178878+abhisharma76@users.noreply.github.com>
2020-03-23 13:08:25 +0530

    Update problem10.hs

--- problem10.hs
+++ problem10.hs
@@ -5,3 +5,6 @@ module Problem10 where
 
 encode :: Eq a => [a] -> [(Int,a)]
 encode xs = map (\ll -> (length ll, head ll)) (pack xs)
+
+test1 = encode [1,1,1,2,2,3,3,3]
+test2 = encode "aaaabbbbbcccc"
